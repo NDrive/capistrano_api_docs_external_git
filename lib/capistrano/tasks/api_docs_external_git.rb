685b02b652280c952c40f070a11a0f69988297ab
Hugo Peixoto <hugo.peixoto@gmail.com>
2015-04-22 17:52:48 +0100

    Uses capistrano logging subsystem

--- lib/capistrano/tasks/api_docs_external_git.rb
+++ lib/capistrano/tasks/api_docs_external_git.rb
@@ -8,9 +8,7 @@ namespace :api_docs_external_git do
   set :api_docs_path, fetch(:rails_root).join("doc", "api")
 
   task :setup do
-    puts "**********************************"
-    puts "Updating API Documentation"
-    puts "**********************************"
+    info "Updating API documentation"
     invoke :'api_docs_external_git:clone_repo'
     invoke :'api_docs_external_git:copy_files'
     invoke :'api_docs_external_git:add_files_git'
@@ -19,7 +17,7 @@ namespace :api_docs_external_git do
 
   desc 'Clone documentation repository to tmp folder'
   task :clone_repo do
-    puts "Cloning #{fetch(:doc_git_repository).to_s} for temporary folder"
+    info "Cloning #{fetch(:doc_git_repository)} for temporary folder"
     @git_repo = Git.clone(fetch(:doc_git_repository), fetch(:doc_repository_name), path: fetch(:rails_root_tmp_path))
   end
 
@@ -30,20 +28,20 @@ namespace :api_docs_external_git do
 
     FileUtils.rm_r destination_folder, force: true
 
-    puts "Copying documentation files"
+    info "Copying documentation files"
     FileUtils.mkdir_p destination_folder
     FileUtils.cp_r files, destination_folder
   end
 
   desc 'Remove documentation repository'
   task :remove_files do
-    puts "Removing documentation repository"
+    info "Removing documentation repository"
     FileUtils.rm_r fetch(:rails_root_tmp_path).join(fetch(:doc_repository_name))
   end
 
   desc 'Add files to documentation git'
   task :add_files_git do
-    puts "Saving documentation files on git repository"
+    info "Saving documentation files on git repository"
 
     @git_repo.add(all: true)
     begin
